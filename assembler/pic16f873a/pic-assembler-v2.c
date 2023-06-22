f3e36b3ff569338a6f0a270b387068914fc361ef
Jacob Long <jacoblong853@gmail.com>
2023-06-21 23:56:52 -0400

    more work on new assembler

--- assembler/pic16f873a/pic-assembler-v2.c
+++ assembler/pic16f873a/pic-assembler-v2.c
@@ -42,8 +42,11 @@ int num_vars = 0;
 // file file descriptors
 FILE* f_read = NULL;
 FILE* f_write = NULL;
+// current line
+int line_num = 1;
 // buffer used to store line
 char* line = NULL;
+unsigned long line_length = 0;
 
 // is the size of the file set and what size
 bool size_set = false;
@@ -55,7 +58,7 @@ uint16_t opcode = 0;
 
 /* parses the command line arguments and sets the appropriate flags
  *
- * also opens the correct files */
+ * also opens the correct files and allocates any needed memory */
 void parse_command_line_args(int argc, char** argv);
 
 /* finds instruction index, returns -1 if not valid instruction */
@@ -71,26 +74,23 @@ void parse_line();
 /* handles all necessary changes when encountering .org 
  *
  * this includes changing the current location in the write file */
-void handle_org();
+void handle_org(char* loc);
 
 /* handles all necessary changes when encountering .const 
  *
  * this includes creating a variable with the right value */
-void handle_const();
+void handle_const(char* var, char* val);
 
 /* handles all necessary changes when encountering .label 
  * 
  * this includes creating a constant that has the same value as the current
  * location in the write file */
-void handle_label();
+void handle_label(char* lab);
 
 /* handles all necessary changes when encountering an instruction
  *
  * this includes forming the opcode, and writing it to the file */
-void handle_instruction();
-
-/* this function will write the current opcode to the output file */
-void write_to_file();
+void handle_instruction(char* instr);
 
 /* gets the index of a variable, returns -1 if it does not exist */ 
 int get_var(char* variable);
@@ -111,6 +111,23 @@ void exit_safely(int code);
 
 int main(int argc, char** argv) {
   parse_command_line_args(argc, argv);
+  do {
+    int rc = getline(&line, &line_length, f_read);
+    line_num++;
+    if (rc < 0) break; // error or EOF, both handled outside loop
+   
+    // parse the line just read in
+    parse_line();
+
+  } while (true);
+  
+  // check for errors in read file
+  if (ferror(f_read) != 0) {
+    perror("ERROR: input file read failure\n");
+    exit_safely(EXIT_FAILURE);
+  } 
+
+  exit_safely(EXIT_SUCCESS);
 }
 
 void parse_command_line_args(int argc, char** argv) {
@@ -149,13 +166,18 @@ void parse_command_line_args(int argc, char** argv) {
       input_file = argv[i];
     }
   }
-
+  
+  // open files
   f_read = fopen(input_file, "r"); // read mode
   f_write = fopen(output_file, "wb"); // binary write mode
   if (f_read == NULL || f_write == NULL) {
     perror("fopen() failed\n");
     exit(EXIT_FAILURE);
   }
+
+  // allocate necessary memory
+  line = (char*)calloc(150, sizeof(char));
+  line_length = 150;
 }
 
 int get_instr(char* instruction) {
@@ -168,22 +190,142 @@ int get_instr(char* instruction) {
 }
 
 void parse_line() {
+  // remove all comments from line
+  for (int i=0; i < strlen(line); i++) {
+    if (line[i] == ';') line[i] = '\0';
+  }
+  
+  char* first = strtok(line, " ");
+  if (first == NULL) return; // line is empty
+  
+  // .org, .const, or .label
+  if (first[0] == '.') {
+    // get next two potential arguments
+    char* second = strtok(NULL, " ");
+    char* third = strtok(NULL, "\n");
+
+    if ( strcmp(first, ".org") == 0 ) {
+      handle_org(second);
+    } else if ( strcmp(first, ".const") == 0 ) {
+      handle_const(second, third);
+    } else if ( strcmp(first, ".label") == 0 ) {
+      handle_label(second);
+    } else {
+      fprintf(stderr, "ERROR: Line %d; unrecognized command %s\n", line_num, first);
+      exit_safely(EXIT_FAILURE);
+    }
+
+  // instruction
+  } else {
+    handle_instruction(first);
+  }
 }
 
-void handle_org() {
-
+void handle_org(char* loc) {
+  // try converting straight to number
+  int x = char_to_num(loc);
+  // try finding variable
+  int i = get_var(loc);
+  // if neither number nor variable, error
+  if (x < 0 && i < 0) {
+    fprintf(stderr, "ERROR: Line %d; .org expects number argument\n", line_num);
+    exit_safely(EXIT_FAILURE);
+  }
+  
+  // if variable, get value
+  if (i > 0) {
+    x = var_values[i];
+  }
+  
+  // go to correct location
+  if (fseek(f_write, x, SEEK_SET) < 0) {
+    perror("fseek() to correct location failed\n");
+    exit_safely(EXIT_FAILURE);
+  }
 }
 
-void handle_const() {
-
+void handle_const(char* var, char* val) {
+  // TODO: Handle constants
 }
 
-void handle_label() {
+void handle_label(char* lab) {
+  if (get_var(lab) >= 0) {
+    fprintf(stderr, "WARNING: Line %d; label overwriting value\n", line_num);
+  }
+  
+  // get current location in file
+  int x = ftell(f_write);
+  if (x < 0) { // check for errors
+    perror("ftell() to find location failed\n");
+    exit_safely(EXIT_FAILURE);
+  }
 
+  // set label to value
+  set_var(lab, x);
 }
 
-void handle_instruction() {
+void handle_instruction(char* instr) {
+  // get index of instruction 
+  int index = get_instr(instr);
+  if (index < 0) {
+    fprintf(stderr, "ERROR: Line %d; instruction not recognized\n", line_num);
+    exit_safely(EXIT_FAILURE);
+  }
 
+  // get args and remove whitespace
+  char* arg1 = strtok(NULL, ",");
+  if (arg1 != NULL) {
+    while (isspace(arg1[0])) arg1++;
+    while (isspace(arg1[strlen(arg1)-1])) arg1[strlen(arg1)-1] = '\0';
+  }
+  char* arg2 = strtok(NULL, "\n");
+  if (arg2 != NULL) {
+    while (isspace(arg2[0])) arg2++;
+    while (isspace(arg2[strlen(arg2)-1])) arg2[strlen(arg2)-1] = '\0';
+  }
+
+  uint8_t instr_args = instruction_args[index];
+
+  if ((instr_args & 0x80) != 0) { // <f> argument expected
+    if (arg1 == NULL) {
+      fprintf(stderr, "ERROR: Line %d; '%s' expects args\n", line_num, instr);
+    }
+    // TODO: Handle getting <f> arg
+
+
+
+    if ((instr_args & 0x40) != 0) { // <d> argument expected
+      if (arg2 == NULL) {
+        fprintf(stderr, "ERROR: Line %d; '%s' expects args\n", line_num, instr);
+      }
+      // TODO: Handle getting <d> arg
+
+    } else if ((instr_args & 0x20) != 0) { // <b> argument expected
+      if (arg2 == NULL) {
+        fprintf(stderr, "ERROR: Line %d; '%s' expects args\n", line_num, instr);
+      }
+      // TODO: Handle getting <b> arg
+
+    }
+
+  } else if ((instr_args & 0x10) != 0) { // <k> argument expected
+    if (arg1 == NULL) {
+      fprintf(stderr, "ERROR: Line %d; '%s' expects args\n", line_num, instr);
+    }
+    // TODO: Handle getting <k> arg
+    
+
+
+    // TODO: Handle properly setting <k> arg
+    if ((instr_args & 0x08) != 0) { // <k> takes 11 bits instead of 8
+
+    } else { // <k> only takes up 8 bits
+
+    }
+  
+  }
+
+  // TODO: write opcode to file
 }
 
 int get_var(char* variable) { 
