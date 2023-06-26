b9cf14edb00907afd617f59dc0c82f01a808d9c1
Jacob Long <jacoblong853@gmail.com>
2023-06-25 23:13:20 -0400

    continue working on programmer

--- assembler/pic16f873a/programmer/src/main.cpp
+++ assembler/pic16f873a/programmer/src/main.cpp
@@ -13,6 +13,26 @@ const int PROGM_DTA     =  3;
 const int MASTR_CLR     =  4;
 const int PROGM_MAN     =  5;
 
+// control whether or not to do anything
+bool started = false;
+// control whether or not we are programming
+bool programming = false;
+// current instr to be programmed
+uint16_t instr;
+// number of bytes recieved through Serial
+unsigned long num_bytes_recieved;
+
+
+/* handles the leds and when they should be on, an led being on means
+ * that the corresponding push-button will recieve an input */ 
+void handle_leds();
+/* erases the entire pic microcontroller */
+void erase_pic();
+/* programs the contents of `instr` to the pic */
+void progm_instr();
+/* programs `val` into the pic */
+void progm_mem(uint8_t val);
+
 
 void setup() {
   Serial.begin(9600);
@@ -42,10 +62,58 @@ void setup() {
 }
 
 void loop() {
+  handle_leds();
+
+  if (!started && digitalRead(START_PIN)) {
+    started = true;
+    Serial.print("Starting...");
+    digitalWrite(PROGM_MAN, HIGH);
+    delay(1);
+    digitalWrite(MASTR_CLR, HIGH);
+    Serial.println("Done");
+  }
+  if (!started) return;
+
+  if (digitalRead(ERASE_PIN) && !programming) {
+    Serial.print("Erasing...");
+    erase_pic();
+    Serial.println("Done");
+  }
+
+  if (digitalRead(PROGM_PIN)) {
+    programming = true;
+    Serial.println("Programming has begun, send data now");
+  }
+  if (!programming) return;
+
   if (Serial.available() > 0) {
+    num_bytes_recieved++;
+    
     int inByte = Serial.read();
 
-    Serial.print("recieved: ");
-    Serial.println(inByte, BIN);
+    if (num_bytes_recieved < 8192) { // still in instruction memory
+      if (num_bytes_recieved % 2 == 1) {
+        instr = inByte << 8;
+      } else {
+        inByte &= 0x000000FF;
+        instr |= inByte;
+        progm_instr();
+      }
+
+    } else { // now in regular memory
+      progm_mem(inByte);
+    }
+  }  
+}
+
+void handle_leds() {
+  digitalWrite(START_LED, LOW);
+  digitalWrite(ERASE_LED, LOW);
+  digitalWrite(PROGM_LED, LOW);
+  if (!started) {
+    digitalWrite(START_LED, HIGH);
+  } else if (!programming) {
+    digitalWrite(ERASE_LED, HIGH);
+    digitalWrite(PROGM_LED, HIGH);
   }
 }
