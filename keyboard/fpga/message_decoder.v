7b69a99214c4e84550a2e62ed05bdea716d23206
Jacob Long <jacoblong853@gmail.com>
2023-07-24 15:56:15 -0400

    further implementation of message decoder

--- keyboard/fpga/message_decoder.v
+++ keyboard/fpga/message_decoder.v
@@ -17,6 +17,33 @@ module message_decoder (
   reg ctrl = 0;
   reg alt = 0;
 
+  /* release_key and extended_code will disappear when next code comes in,
+  * which is guaranteed, so need to save them */
+  reg released = 0;
+  reg extended = 0;
+
   reg [7:0] scancode_to_ascii[256];
+
+  always @(posedge release_key) begin
+    released <= 1;
+  end
+
+  always @(posedge extended_code) begin
+    extended <= 1;
+  end
+
+  always @(posedge scancode_valid) begin
+    if ((scancode == 8'h12 || scancode == 8'h59) && ~extended) begin
+      shift <= ~released;
+    end else if (scancode == 8'h58 && ~extended && ~released) begin
+      caps_lock <= ~caps_lock;
+    end else if (scancode == 8'h14) begin  // extended or not
+      ctrl <= ~released;
+    end else if (scancode == 8'h11) begin  // extended or not
+      alt <= ~released;
+    end else begin
+      ascii <= scancode_to_ascii[scancode];
+    end
+  end
 
 endmodule
