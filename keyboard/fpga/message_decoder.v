5aaefc790673a7d444f4b5dffccb4d2ebbe2cd8b
Jacob Long <jacoblong853@gmail.com>
2023-07-24 13:37:52 -0400

    start implementing the message_decoder

---
+++ keyboard/fpga/message_decoder.v
@@ -0,0 +1,20 @@
+/*
+* Converts the ps/2 scancode input into ascii character output
+*/
+
+module message_decoder (
+    input [7:0] scancode,
+    input       scancode_valid,
+    input       release_key,
+    input       extended_code,
+
+    output reg [7:0] ascii,
+    output reg       ascii_valid
+);
+
+  reg shift = 0;
+  reg caps_lock = 0;
+  reg ctrl = 0;
+  reg alt = 0;
+
+endmodule
