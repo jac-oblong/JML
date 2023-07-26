265df742e5a706a4466521a3c35732e574714377
Jacob Long <jacoblong853@gmail.com>
2023-07-26 10:42:07 -0400

    add more to message_decoder

--- keyboard/fpga/message_decoder.v
+++ keyboard/fpga/message_decoder.v
@@ -1,5 +1,8 @@
 /*
-* Converts the ps/2 scancode input into ascii character output
+* Converts the ps/2 scancode input into ascii character output.
+* Also will reset keybard if required.
+* Outputs the ascii symbol, as well as two modifier keys (ctrl and alt). When
+* a new output is available, ascii_valid will toggle.
 */
 
 module message_decoder (
@@ -7,7 +10,7 @@ module message_decoder (
     input       scancode_valid,
 
     output reg [7:0] ascii,
-    output reg       ascii_valid,
+    output reg       ascii_valid,     // will toggle on valid output
     output           reset_required,
     output reg       ctrl,
     output reg       alt
@@ -24,49 +27,53 @@ module message_decoder (
   // similar to using a rom instead of a complex logic circuit
   reg [7:0] scancode_to_ascii[256];
 
+  initial $readmemh("scancode_to_ascii.txt", scancode_to_ascii);
+
 
   assign reset_required = (scancode == 8'hAA);
 
   always @(posedge scancode_valid) begin
     case (scancode)
-      8'hF0: released <= 1;
+      8'hF0: released <= 1;  // released key
 
-      8'hE0: extended <= 1;
+      8'hE0: extended <= 1;  // extended code
 
-      8'h11: begin
+      8'h11: begin  // alt key
         alt <= ~released;  // extended does not matter
         extended <= 0;
         released <= 0;
       end
 
-      8'h14: begin
+      8'h14: begin  // ctrl key
         ctrl <= ~released;  // extended does not matter
         extended <= 0;
         released <= 0;
       end
 
-      8'h12: begin
+      8'h12: begin  // shift key
         if (~extended) begin
           shift <= ~released;
           released <= 0;
         end
       end
 
-      8'h59: begin
+      8'h59: begin  // another shift key
         if (~extended) begin
           shift <= ~released;
           released <= 0;
         end
       end
 
-      8'h58: begin
+      8'h58: begin  // caps lock
         if (~extended && ~released) begin
           caps_lock <= ~caps_lock;
         end
       end
 
-      default: begin
-        ascii <= scancode_to_ascii[scancode];
+      default: begin  // normal character
+        ascii <= (shift || caps_lock) ? scancode_to_ascii[scancode + 128] :
+            scancode_to_ascii[scancode];
+        ascii_valid <= ~ascii_valid;
       end
 
     endcase
