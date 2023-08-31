972e6595cade7964ac3309ad973cf0f4372625c9
Jacob Long <jacoblong853@gmail.com>
2023-08-31 10:32:44 -0400

    more work on rom-utilities-ui

--- related-projects/rom-utilities-ui/rom-utilities-ui.c
+++ related-projects/rom-utilities-ui/rom-utilities-ui.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdint.h>
@@ -6,6 +7,8 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
+
+#define EEPROM_SIZE 8192
 
 // reads contents of EEPROM into file
 void read_to_file(char *filename, int serial_fd);
@@ -23,21 +26,7 @@ int main() {
 
   // get user response
   char *filename = calloc(256, sizeof(char));
-  if (!fgets(filename, 256, stdin)) {
-    fprintf(stderr, "reading user input failed\n");
-    free(filename);
-    return EXIT_FAILURE;
-  }
-  // if full path not obtained, continue getting more
-  while (filename[strlen(filename) - 1] != '\n') {
-    filename = realloc(filename, strlen(filename) + 256);
-    if (!fgets(filename + strlen(filename), 256, stdin)) {
-      fprintf(stderr, "reading user input failed\n");
-      free(filename);
-      return EXIT_FAILURE;
-    }
-  }
-  filename[strlen(filename) - 1] = '\0';
+  read_input(&filename);
 
   // open user provided file
   int serial_port = open(filename, O_RDWR);
@@ -140,4 +129,105 @@ int main() {
 
   close(serial_port);
   return EXIT_SUCCESS;
+}
+
+void read_to_file(char *filename, int serial_fd) {
+  // open the file
+  int fd = open(filename, O_WRONLY | O_CREAT | O_EXCL);
+  if (fd < 0) {
+    perror("failed to open file to read EEPROM into");
+    return;
+  }
+
+  uint16_t address = 0;
+  while (address < EEPROM_SIZE) {
+    // tell Arduino we want to read
+    uint8_t read_op = 0x00;
+    write(serial_fd, &read_op, sizeof(read_op));
+
+    // send address to read from
+    uint8_t high_byte = address >> 8;
+    uint8_t low_byte = address;
+    write(serial_fd, &high_byte, sizeof(high_byte));
+    write(serial_fd, &low_byte, sizeof(low_byte));
+
+    // get 16 byte response
+    int num_nonresponsive = 0;
+    int should_exit = 0;
+    int i = 0;
+    uint8_t buf[16];
+    while (i != 16) {
+      // read will block for 1 sec
+      int n = read(serial_fd, buf + i, sizeof(uint8_t));
+      if (n < 0) {
+        perror("read() failed");
+        should_exit = 1;
+        break;
+      }
+
+      i += n;
+      if (n == 0) {
+        num_nonresponsive++;
+      }
+
+      if (num_nonresponsive == 25) {
+        // 25 sec with no response
+        fprintf(stderr, "Arduino is not responding, exiting\n");
+        should_exit = 1;
+        break;
+      }
+    }
+    if (should_exit == 1) {
+      break;
+    }
+
+    // write data to file (in human readable form)
+    char line[256];
+    sprintf(line,
+            "%02hhX %02hhX %02hhX %02hhX %02hhX %02hhX %02hhX %02hhX\t\t%02hhX "
+            "%02hhX %02hhX %02hhX %02hhX %02hhX %02hhX %02hhX\n",
+            buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7],
+            buf[8], buf[9], buf[10], buf[11], buf[12], buf[13], buf[14],
+            buf[15]);
+    write(fd, line, strlen(line));
+
+    // increment address
+    address += 16;
+  }
+
+  close(fd);
+}
+
+void write_from_file(char *filename, int serial_fd) {}
+
+int strcmpcaseins(char *str1, char *str2) {
+  if (strlen(str1) != strlen(str2)) {
+    return -1;
+  }
+
+  for (int i = 0; i < strlen(str1); i++) {
+    if (tolower(str1[i]) != tolower(str2[i])) {
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+void read_input(char **buf) {
+  if (!fgets(*buf, 256, stdin)) {
+    fprintf(stderr, "reading user input failed\n");
+    free(*buf);
+    exit(EXIT_FAILURE);
+  }
+  // if full path not obtained, continue getting more
+  while (*buf[strlen(*buf) - 1] != '\n') {
+    *buf = realloc(*buf, strlen(*buf) + 256);
+    if (!fgets(*buf + strlen(*buf), 256, stdin)) {
+      fprintf(stderr, "reading user input failed\n");
+      free(*buf);
+      exit(EXIT_FAILURE);
+    }
+  }
+  *buf[strlen(*buf) - 1] = '\0';
 }
