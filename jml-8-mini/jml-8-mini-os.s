f68015431336aedcf22487d4374e11f380b6950c
Jacob Long <jacoblong853@gmail.com>
2023-09-12 11:08:31 -0400

    some minor changes to ram tester

--- jml-8-mini/jml-8-mini-os.s
+++ jml-8-mini/jml-8-mini-os.s
@@ -5,12 +5,13 @@ org 0x0000
 
   LD B, 0x00                    ; value with which to test RAM
 outer_loop:
-  LD (HL), 0x8000               ; start at upper address of memory
+  LD (HL), 0x8000               ; start at lower address of RAM
 
 inner_loop:
   LD (HL), B                    ; store value in memory
   LD A, (HL)                    ; retrieve value from mem
-  ;; THIS IS WHERE 'CP B' and 'JP NZ, ...' would go when testing
+  CP B                          ; compare A to B
+  ;; THIS IS WHERE 'JP NZ, ...' would go when testing
 
   INC (HL)                      ; go to next location
   JR NZ, inner_loop
@@ -18,4 +19,5 @@ inner_loop:
   INC B                         ; go to next value for test if B NZ
   JR NZ, outer_loop
 
+end:
   HALT                          ; stop running
