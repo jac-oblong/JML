ecc4d0c3ab66f2240d5eaa50b26122fc1d713259
Jacob Long <jacoblong853@gmail.com>
2023-09-12 22:15:31 -0400

    small script to test RAM

--- jml-8-mini/jml-8-mini-os.s
+++ jml-8-mini/jml-8-mini-os.s
@@ -3,21 +3,23 @@
 
 org 0x0000
 
-  LD B, 0x00                    ; value with which to test RAM
-outer_loop:
-  LD (HL), 0x8000               ; start at lower address of RAM
+  LD B, 0x55                    ; value with which to test RAM
+  LD HL, 0xFFFF                 ; start at high address of RAM
 
-inner_loop:
+loop:
   LD (HL), B                    ; store value in memory
   LD A, (HL)                    ; retrieve value from mem
   CP B                          ; compare A to B
-  ;; THIS IS WHERE 'JP NZ, ...' would go when testing
+  JR NZ, error
 
-  INC (HL)                      ; go to next location
-  JR NZ, inner_loop
-
-  INC B                         ; go to next value for test if B NZ
-  JR NZ, outer_loop
+  DEC HL                        ; go to next location
+  LD A, 0x7F                    ; load A with high value stop
+  CP H                          ; compare A to H
+  JR NZ, loop             ; if not equal, continue
+                                ; equal, so passed 0x8000 boundary of mem
 
 end:
   HALT                          ; stop running
+
+error:
+  JP error
