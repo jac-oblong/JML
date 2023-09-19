9e90c8ed5c432844975437b30f2cd7b58eda1f1c
Jacob Long <jacoblong853@gmail.com>
2023-09-19 10:38:14 -0400

    yeah

--- jml-8-mini/jml-8-mini-os.s
+++ jml-8-mini/jml-8-mini-os.s
@@ -1,25 +1,10 @@
-  ;; 'OS' for the jml-8 mini computer
-  ;; currently just tests RAM (0x8000 - 0xFFFF)
+;; 'OS' for the jml-8 mini computer
 
 org 0x0000
 
-  LD B, 0x55                    ; value with which to test RAM
-  LD HL, 0xFFFF                 ; start at high address of RAM
 
-loop:
-  LD (HL), B                    ; store value in memory
-  LD A, (HL)                    ; retrieve value from mem
-  CP B                          ; compare A to B
-  JR NZ, error
-
-  DEC HL                        ; go to next location
-  LD A, 0x7F                    ; load A with high value stop
-  CP H                          ; compare A to H
-  JR NZ, loop             ; if not equal, continue
-                                ; equal, so passed 0x8000 boundary of mem
-
-end:
-  HALT                          ; stop running
-
-error:
-  JP error
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; INITIALIZE CTC ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
