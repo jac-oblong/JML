2828fe587599473d93cff789a82000b0bbe754a0
Jacob Long <jacoblong853@gmail.com>
2023-09-26 00:10:08 -0400

    reformatting of code, testing sending all characters over uart

--- jml-8-mini/jml-8-mini-os.s
+++ jml-8-mini/jml-8-mini-os.s
@@ -36,11 +36,13 @@ CTC_TIMER1: equ 0x09
 CTC_TIMER2: equ 0x0A
 CTC_TIMER3: equ 0x0B
 
-;; FIFO BUFFER FOR HOLDING INPUT DATA (0x8000-0x80FF) (memory address 0x8100
-;; holds current size of buffer) (this buffer is rolling, so the 'base' may
-;; change while the computer is running)
+;; FIFO BUFFER FOR HOLDING INPUT DATA (0x8000-0x80FD) (memory address 0x80FF
+;; holds where the tail of the buffer is and where data should be read from,
+;; 0x80FE holds where the head of the buffer is and where new data should be
+;; put) (this buffer is rolling)
 RX_DATA_BASE: equ 0x8000
-RX_DATA_CURR: equ 0x8100
+RX_DATA_HEAD: equ 0x80FE    ; only to be written to by an interrupt
+RX_DATA_TAIL: equ 0x80FF    ; only to be written to by _main
 
 ;; DEFAULT STARTING LOCATION OF STACK
 STACKSTART: equ 0x0000
@@ -69,43 +71,38 @@ _init_ctc:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 _init_sio:
-  ld A, 0x30                    ; reset any errors, select WR0
-  out (SIO_A_CONT), A
-  ld A, 0x18                    ; reset channel A
-  out (SIO_A_CONT), A
+  jp continue_sio_init          ; jump over this section of data
+SIO_A_INIT_DATA_BEGIN:
+  db 0x30                       ; reset any errors, select WR0
+  db 0x18                       ; reset channel A
+  db 0x04                       ; select WR4
+  db 0x44                       ; no parity, 1 stop bit, 8 data bits, x16 clock
+  db 0x05                       ; select WR5
+  db 0xE8                       ; dtr active, enable tx, 8 data bits
+  db 0x01                       ; select WR1
+  db 0x10                       ; interrup on all rx characters, parity error is
+                                ; special condition
+  db 0x03                       ; enable rx, 8 bits per character
+SIO_A_INIT_DATA_SIZE: equ $-SIO_A_INIT_DATA_BEGIN
+SIO_B_INIT_DATA_BEGIN:
+  db 0x01                       ; select WR1
+  db 0x04                       ; no interrupt in chb, spec rx condition affects
+                                ; interrupt vector
+  db 0x02                       ; select WR2
+  db 0xF0                       ; load interrupt vector with lower 8 bits
+                                ; (lowest 4 will be changed depending on vector)
+SIO_B_INIT_DATA_SIZE: equ $-SIO_B_INIT_DATA_BEGIN
 
-  ld A, 0x04                    ; select WR4
-  out (SIO_A_CONT), A
-  ld A, 0x45                    ; odd parity, 1 stop bit, 8 data bits, x1 clock
-  out (SIO_A_CONT), A
+continue_sio_init:
+  ld C, SIO_A_CONT              ; load C with location of sio cha control
+  ld B, SIO_A_INIT_DATA_SIZE    ; load B with number of bytes to load
+  ld HL, SIO_A_INIT_DATA_BEGIN  ; load HL with beginning of data
+  otir
 
-  ld A, 0x05                    ; select WR5
-  out (SIO_A_CONT), A
-  ld A, 0xE8                    ; dtr active, enable tx, 8 data bits
-  out (SIO_A_CONT), A
-
-  ld A, 0x01                    ; select WR1
-  out (SIO_B_CONT), A
-  ld A, 0x04                    ; no interrupt in channel b, special rx conditon
-                                ; affects int vec
-  out (SIO_B_CONT), A
-
-  ld A, 0x02                    ; select WR2
-  out (SIO_B_CONT), A
-  ld A, 0xF0                    ; load interrupt vector (lower 8 bits, lowest 4
-                                ; will be changed to match vector)
-  out (SIO_B_CONT), A
-
-  ld A, 0x01                    ; select WR1
-  out (SIO_A_CONT), A
-  ld A, 0x10                    ; interrupt on all rx characters, parity error
-                                ; is special condition
-  out (SIO_A_CONT), A
-
-  ld A, 0x03                    ; select WR1
-  out (SIO_A_CONT), A
-  ld A, 0xC1                    ; enable rx, 8 bits per character
-  out (SIO_A_CONT), A
+  ld C, SIO_B_CONT              ; repeat for chb
+  ld B, SIO_B_INIT_DATA_SIZE
+  ld HL, SIO_B_INIT_DATA_BEGIN
+  otir
 
   ;; SIO channel A is now active, ready for use with UART
 
@@ -122,9 +119,15 @@ _main:
   im 2                          ; set interrupt mode to 2
   ei                            ; enable interrupts
 
-loop:
-  jp loop
-
+send_all_chars_loop:
+  ld A, 0x20                    ; load A with ascii for ' '
+send_a_char:
+  out (SIO_A_DATA), A           ; send to uart
+  call f_uart_block_tx_empty
+  inc A                         ; go to next ascii value
+  bit 7, A                      ; check if bit 7 if set in A
+  jp z, send_a_char             ; if not overflow, send next char
+  jp send_all_chars_loop
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -158,7 +161,7 @@ f_uart_block_tx_empty:
   in A, (SIO_A_CONT)            ; read value of RR1
   bit 0, A                      ; test bit zero of A, will be 0 when empty
 
-  jp Z, f_uart_block_tx_empty
+  jp z, f_uart_block_tx_empty
   ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -170,18 +173,29 @@ f_uart_block_tx_empty:
 __uart_rx_available:
   ;; retrieves received data from uart and adds to input buffer
   di                            ; disable interrupts
+  ex AF, AF'                    ; exchange all registers
+  exx
+
   call f_uart_rts_off           ; tell host to not send more data
 
   ld HL, RX_DATA_BASE           ; set base of input buffer
-  ld A, (RX_DATA_CURR)          ; get current location of input buffer
+  ld A, (RX_DATA_HEAD)          ; get current location of input buffer
   inc A
-  ld (RX_DATA_CURR), A          ; increment top of buffer
+  cp 0xFE                       ; upper limit of input buffer
+  jp NZ, store_byte
+  ld A, 0x00
+
+store_byte:
+  ld (RX_DATA_HEAD), A          ; store new location of HEAD
   ld L, A                       ; HL now has location where new byte should go
 
   in A, (SIO_A_DATA)            ; read rx byte and store in buffer
   ld (HL), A
 
   call f_uart_rts_on            ; allow for more data to be sent
+
+  ex AF, AF'                    ; exchange registers again
+  exx
   ei
   reti
 
